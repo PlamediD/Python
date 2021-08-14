58d1e6cec4b3896bce22429453bed890287c7f33
Plamedi K Diakubama <87151146+PlamediD@users.noreply.github.com>
2021-08-13 18:44:02 -0700

    Add files via upload

--- bubbleSort_Option2.py
+++ bubbleSort_Option2.py
@@ -1,11 +1,15 @@
-list=[1,5,3,2,0,8]
-
+#Bubble Sort
 def bubbleSort(list):
+    count=0
     for j in range(len(list)-1):
         for i in range(len(list)-1-j):
+            count+=1
             if list[i]>list[i+1]:
                 list[i],list[i+1]=list[i+1],list[i]
-                
+               
+
+    print("Number of evalutions: ",count)           
     return list
-    
-print(bubbleSort(list))
+list=[1,5,3,2,0,8]
+print("Given unsorted list is: ",list)   
+print("Sorted list: ",bubbleSort(list))
